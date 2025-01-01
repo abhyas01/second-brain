afe8506567ee04a54e8abf05bef2b8ca0ea893eb
Abhyas Mall <mallabhyas@gmail.com>
2025-01-01 15:02:57 -0800

    frontend-skeleton-added

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import Button from "./components/Button";
+
+function App() {
+
+  return (
+    <>
+    <Button variant="primary" size="sm" text="Share" onClick={() => {}} />
+    <Button variant="secondary" size="md" text="Add Content" onClick={() => {}} />
+    <Button variant="secondary" size="lg" text="Add Content" onClick={() => {}} />
+    </>
+  );
+}
+
+export default App
